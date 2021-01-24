7eef67e4be0496014ccf5cc8f005f5fab26f9b3d
kelechi <oparaiskelechi@gmail.com>
2021-01-24 17:31:44 +0100

    initail commit--- done with the Room model creation

---
+++ app/src/main/java/com/example/contactroom/db/SubscriberDAO.kt
@@ -0,0 +1,24 @@
+package com.example.contactroom.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface SubscriberDAO {
+
+    @Insert
+    suspend fun insertSubscriber(subscriber: Subscriber): Long
+
+    @Update
+    suspend fun updateSubscriber(subscriber: Subscriber)
+
+    @Delete
+    suspend fun deleteSubscriber(subscriber: Subscriber)
+
+    @Query("DELETE FROM subscriber_data_table")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM subscriber_data_table")
+    fun getAllSubscriber(): LiveData<List<Subscriber>>
+
+}
